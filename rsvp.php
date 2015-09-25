9e5af9cb821772b2e4bfe85f65b672e691560729
jm2498 <jm2498@cornell.edu>
2015-09-24 22:40:13 -0400

    Modified php file

--- rsvp.php
+++ rsvp.php
@@ -1,5 +1,5 @@
 <?php
-	echo "Hello World";
+	echo "<script>alert("Hello World");</script>";
 	$msg = "no"; 
     $name     = $_POST['name'];
     $email    = $_POST['email'];
@@ -7,23 +7,23 @@
     $wedding    = $_POST['wedding'];
     
     if($name !='' && $email!='' && $persons !='' && $wedding !=''){
-        $to = "jiangyumao@gmail.com";
-	$subject = "RSVP Mail";
-	$message = "<p>Dear Admin,</p><p>You have a RSVP request </p><p>Please find the below information</p>
-                    <p>Name :".$name."</p>
-                    <p>Email :".$email."</p>
-                    <p>Number of Persons Attanding :".$persons."</p>
-                    <p>Event :".$wedding."</p>";
-	$header .= 'To: '.$name.' '.$email. "\r\n";
-	$header  = "From:".$email."\r\n";
-	$header .= "MIME-Version: 1.0\r\n";
-	$header .= "Content-type: text/html\r\n";
-	$send = mail ($to,$subject,$message,$header);
-        if($send){
-	$msg = "ok"; 
-	} else{
-	$msg = "no"; 
-	}
+	    $to = "jiangyumao@gmail.com";
+		$subject = "RSVP Mail";
+		$message = "<p>Dear Admin,</p><p>You have a RSVP request </p><p>Please find the below information</p>
+	                    <p>Name :".$name."</p>
+	                    <p>Email :".$email."</p>
+	                    <p>Number of Persons Attanding :".$persons."</p>
+	                    <p>Event :".$wedding."</p>";
+		$header .= 'To: '.$name.' '.$email. "\r\n";
+		$header  = "From:".$email."\r\n";
+		$header .= "MIME-Version: 1.0\r\n";
+		$header .= "Content-type: text/html\r\n";
+		$send = mail ($to,$subject,$message,$header);
+	    if($send){
+		$msg = "ok"; 
+		} else{
+		$msg = "no"; 
+		}
         echo $msg;
     }
 	else
